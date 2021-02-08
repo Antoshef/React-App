3e305696181ec3f6c7807912faf576f52c943559
Anton Stanev <antoshef21@gmail.com>
2021-02-08 20:35:26 +0200

    react

--- react-guide/src/Person/Person.js
+++ react-guide/src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Person = ( props ) => {
-    return <div>
+    return <div className="Person">
                 <p onClick= { props.click }>I'm { props.name }, and I am a { props.age } years old!</p>
                 <p>{ props.children }</p>
                 <input type="text" onChange= { props.change } value= { props.value }/>
