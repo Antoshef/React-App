24195d51c309ce4739479db13aa330f4eff4f117
Anton Stanev <antoshef21@gmail.com>
2021-02-17 19:05:59 +0200

    burger

---
+++ Burger-Builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxilary';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Burger Controler</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
