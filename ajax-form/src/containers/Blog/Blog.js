5f4563dc2e9a2d720bb55f8faf9da275a49a557f
Anton Stanev <antoshef21@gmail.com>
2021-02-23 21:37:18 +0200

    ajax

--- ajax-form/src/containers/Blog/Blog.js
+++ ajax-form/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -12,8 +12,10 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/new-post">New Post</Link></li>
+                            <li><NavLink to="/" exact 
+                                activeStyle={{color: 'orange', textDecoration: 'underline'}}>
+                                Home</NavLink></li>
+                            <li><NavLink to="/new-post">New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
