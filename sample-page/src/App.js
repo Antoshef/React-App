6ac77abc935c24e2907147770445d63742dfa994
Anton Stanev <antoshef21@gmail.com>
2021-02-27 23:46:52 +0200

    sample

--- sample-page/src/App.js
+++ sample-page/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>You  are the man</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
