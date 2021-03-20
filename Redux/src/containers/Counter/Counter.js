f4c6b8bf11644451d95eed364604ecd50c1aaec0
Anton Stanev <antoshef21@gmail.com>
2021-03-21 00:30:02 +0200

    redux

--- Redux/src/containers/Counter/Counter.js
+++ Redux/src/containers/Counter/Counter.js
@@ -29,8 +29,8 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
                 <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
@@ -39,4 +39,16 @@ class Counter extends Component {
     }
 }
 
-export default connect(Counter);
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'})
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
