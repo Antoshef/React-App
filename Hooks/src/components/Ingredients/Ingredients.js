09ecceb85abca627e0ec5291facc88f49d696a99
Anton Stanev <antoshef21@gmail.com>
2021-03-20 13:47:09 +0200

    hooks

--- Hooks/src/components/Ingredients/Ingredients.js
+++ Hooks/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState, useReducer } from 'react';
+import React, { useCallback, useEffect, useReducer, useState } from 'react';
 import ErrorModal from '../UI/ErrorModal';
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -17,15 +17,29 @@ const ingredientReducer = (currentIngredients, action) => {
   }
 }
 
+const httpReducer = (curHttpState, action) => {
+  switch (action.type) {
+    case 'SEND':
+      return { loading: true, error: null }
+    case 'RESPONSE':
+      return { ...curHttpState, loading: false }
+    case 'ERROR':
+      return { loading: false, error: action.errorData }
+    default:
+      throw new Error('Should not get there!');
+  }
+}
+
 const Ingredients = () => {
   const [userIngredients, dispatch] = useReducer(ingredientReducer, [])
+  const [httpState, dispatchHttp] = useReducer(httpReducer, { loading: false, error: null });
   // const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
     // setUserIngredients(filteredIngredients);
-    dispatch({type: 'SET', ingredients: filteredIngredients});
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
   }, []);
 
   const addIngredientHandler = ingredient => {
@@ -43,7 +57,7 @@ const Ingredients = () => {
         //   ...prevIngredients,
         //   { id: resData.name, ...ingredient }
         // ]);
-        dispatch({type: 'ADD', ingredient: {id: resData.name, ...ingredient}});
+        dispatch({ type: 'ADD', ingredient: { id: resData.name, ...ingredient } });
       });
   };
 
@@ -55,7 +69,7 @@ const Ingredients = () => {
       .then(res => {
         setIsLoading(false);
         // setUserIngredients(prevIngredients => prevIngredients.filter(ig => ig.id !== id));
-        dispatch({type: 'DELETE', id: id})
+        dispatch({ type: 'DELETE', id: id })
       })
       .catch(error => {
         setError('Stupid error!');
