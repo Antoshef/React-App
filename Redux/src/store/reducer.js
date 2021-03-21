0d1296f9c3e06b391510fb913ccecc422b6a36cb
Anton Stanev <antoshef21@gmail.com>
2021-03-21 09:14:55 +0200

    redux

--- Redux/src/store/reducer.js
+++ Redux/src/store/reducer.js
@@ -6,6 +6,12 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return { ...state, counter: state.counter + 1 }
+    case 'DECREMENT':
+      return { ...state, counter: state.counter - 1 }
+    case 'ADD':
+      return { ...state, counter: state.counter + 5 }
+    case 'SUBTRACT':
+      return { ...state, counter: state.counter - 5 }
   }
   return state;
 };
